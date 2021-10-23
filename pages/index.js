bb63be9edf01cb762ce795bced4afa9e988a2233
Harsh <57228178+harsh3294@users.noreply.github.com>
2021-10-23 13:14:02 +0530

    Initialized the app

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+        <h2 className="text-xl font-semibold pb-5">Explore Nearby</h2>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
+    </div>
+  );
+}
