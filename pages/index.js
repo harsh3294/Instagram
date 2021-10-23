5b7d29fa39abfc510fe6a8fa1ab894b9e8a2b068
Harsh <57228178+harsh3294@users.noreply.github.com>
2021-10-23 14:25:38 +0530

    Created Header

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,15 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="">
-        <h2 className="text-xl font-semibold pb-5">Explore Nearby</h2>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Header />
+      <main></main>
     </div>
   );
 }
